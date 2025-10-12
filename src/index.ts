f35e072ff3f0a46655d48017fa234a477ecc303f
Maby <maby200@gmail.com>
2025-10-11 20:26:30 -0500

    feat: add MongoClient with mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,28 @@ import { serve } from "@hono/node-server";
 import app from "./app.js";
 import { GetUsersController } from "./controllers/GetUsers/getUsers.js";
 import { MongoGetUsersRepository } from "./repositories/getUsers/mongoGetUsers.js";
+import { MongoClient } from "./database/mongo.js";
+import { config } from "dotenv";
 
-app.get("/users", async (c) => {
-    const mongoGetUsersRepository = new MongoGetUsersRepository();
-    const getUsersController = new GetUsersController(mongoGetUsersRepository);
-    const response = await getUsersController.handle();
+const main = async () => {
+    config();
+    await MongoClient.connect();
+
+    app.get("/users", async (c) => {
+        const mongoGetUsersRepository = new MongoGetUsersRepository();
+        const getUsersController = new GetUsersController(mongoGetUsersRepository);
+        const response = await getUsersController.handle();
+        
+        return c.json(response);
+    });
     
-    return c.json(response);
-});
+    const port = parseInt(process.env.PORT || '3000');
+    serve({
+        fetch: app.fetch,
+        port,
+    }, (info) => {
+        console.log(`Server running on http://localhost:${info.port}`);
+    });
+}
 
-const port = parseInt(process.env.PORT || '3000');
-
-serve({
-    fetch: app.fetch,
-    port,
-}, (info) => {
-    console.log(`Server running on http://localhost:${info.port}`);
-});
+main();
