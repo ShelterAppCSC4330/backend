7336d2416a4fb9fae39b58bec9333d0a35b6a332
Maby <maby200@gmail.com>
2025-11-21 12:59:02 -0600

    feat: add password hashing for Create/Update user

--- src/repositories/updateUsers/mongoUpdateUsers.ts
+++ src/repositories/updateUsers/mongoUpdateUsers.ts
@@ -5,6 +5,7 @@ import {
 } from "../../controllers/updateUsers/protocols.js";
 import { MongoClient } from "../../database/mongo.js";
 import { User } from "../../models/user.model.js";
+import * as argon2 from 'argon2';
 
 export class MongoUpdateUserRepository implements IUpdateUserRepository {
   async updateUser(id: string, params: UpdateUserParams): Promise<User> {
@@ -22,15 +23,26 @@ export class MongoUpdateUserRepository implements IUpdateUserRepository {
       }
     }
 
+    const updateData: any = { ...params };
+    if (params.password) {
+      try {
+        updateData.password = await argon2.hash(params.password, {
+          type: argon2.argon2id,
+          memoryCost: 2 ** 16,
+          timeCost: 3,
+          parallelism: 1
+        });
+      } catch (error) {
+        throw new Error("Failed to hash password");
+      }
+    }
+
     await MongoClient.db.collection("users").updateOne(
       { _id: new ObjectId(id) },
       {
-        $set: {
-          ...params,
-        },
+        $set: updateData,
       }
     );
-
     const user = await MongoClient.db
       .collection<Omit<User, "id">>("users")
       .findOne({ _id: new ObjectId(id) });
